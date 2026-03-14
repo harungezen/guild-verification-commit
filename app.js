7c4dacd65d6938024d7ad35acfe5d71f282bc88e
harungezen <158600564+harungezen@users.noreply.github.com>
2026-03-15 01:46:55 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:46:42.826Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-14T22:46:54.734Z
+Commit: Fix CSS styling bug on mobile devices
