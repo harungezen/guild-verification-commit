1e81a0270ce93f8adb9e3c805517f7f018456513
harungezen <158600564+harungezen@users.noreply.github.com>
2026-03-15 02:01:41 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T23:01:34.450Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T23:01:40.758Z
+Commit: Patch API endpoint error handling
