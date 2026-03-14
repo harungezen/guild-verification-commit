83ac9a6eee75a079678016759278701e11687195
harungezen <158600564+harungezen@users.noreply.github.com>
2026-03-15 02:01:42 +0300

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T23:01:23.762Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-14T23:01:42.726Z
+Commit: Add dark mode support
