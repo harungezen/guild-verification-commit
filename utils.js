b6b2b7c28b81bc409b72e21f684c6adb8ec57e75
harungezen <158600564+harungezen@users.noreply.github.com>
2026-03-15 01:46:38 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T22:43:56.025Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-14T22:46:38.461Z
+Commit: Fix memory leak in event handlers
