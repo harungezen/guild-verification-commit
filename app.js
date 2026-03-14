a52f394ea63aacacfe461e1d9f310a16a4997e2f
harungezen <158600564+harungezen@users.noreply.github.com>
2026-03-15 00:59:45 +0300

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T21:59:39.970Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T21:59:44.991Z
+Commit: Fix memory leak in event handlers
